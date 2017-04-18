f6e3546c42217f751eb9ed5e08784d10b47f3b25
Eric Wang <eric.myspace@hotmail.com>
2017-04-18 17:02:30 -0400

    add index page and script

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,5 +1,37 @@
 var app = angular.module('TracademicHub', ['ngRoute']);
 
 app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'templates/homepage.html',
+            controller: 'homepageController'
+        })
+        .when('/dashboard', {
+            templateUrl: 'templates/dashboard.html',
+            controller: 'dashboardController'
+        })
+        // .when('/someRoute', {
+        //     templateUrl: 'templates/someRoute.html',
+        //     controller: 'someController'
+        // })
+        // .when('/someRoute/:id', {
+        //     templateUrl: 'templates/someRoute.html',
+        //     controller: 'someDetailedController'
+        // })
+        .otherwise('/');
+});
 
+app.factory('_CheckAuthentication', function () {
+    return {
+        isAuthenticated: function () {
+            return true;
+        },
+        getAccessLevel: function () {
+            if (!this.isAuthenticated()) {
+                return -1;
+            } else {
+                return 50;
+            }
+        }
+    };
 });
