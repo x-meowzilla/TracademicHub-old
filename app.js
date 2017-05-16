97d9d2dbcc6ad46681994b7f7e59ecbab2b3e2b0
Eric Wang <meowster.eric@hotmail.com>
2017-05-16 10:56:48 -0400

    rebuild nodejs server side structure

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
 var express = require('express');
-// var expressValidator = require('express-validator');
+var expressValidator = require('express-validator');
 var path = require('path');
 var favicon = require('serve-favicon');
 var bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var http = require('http');
 // var passport = require('passport');
-
-
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
-
-// app.get('/', function (req, res) {
-//     res.redirect('/index.html');
-// });
-
-
-app.use(express.static('public'));
-
-app.set('port', 3000);
-http.createServer(app).listen(app.get('port'), function () {
-    console.log("Express is running.");
-    console.log('HTTP on port ' + app.get('port'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
     console.log("HTTP request", req.method, req.url, req.body);
@@ -37,3 +22,5 @@ app.use(function (req, res, next) {
 app.use(function (req, res) {
     console.log("HTTP Response", res.statusCode);
 });
+
+module.exports = app;
