8569a45739deb800442570422478747c24b316aa
Eric Wang <meowster.eric@hotmail.com>
2017-05-19 19:21:33 -0400

    fix some server config data

--- app/configurations/server_config.js
+++ app/configurations/server_config.js
@@ -2,12 +2,13 @@ module.exports = {
 
     env: 'development',
     port: this.env === 'production' ? 443 : 3000,
-    httpsCert: 'certificates/cert.pem',
-    httpsKey: 'certificates/key.pem',
+    httpsCert: './certificates/cert.pem',
+    httpsKey: './certificates/key.pem',
     session: {
         key: 'tracademichub.sess', //what the name that the session cookie is stored as
+        secret: '',
         // secret: 'wqcVwJ6rNgrpEpZBppGt', //DO NOT LEAK - change this!!
-        timeout: 12 * 60 * 60 * 1000
+        timeout: 2 * 60 * 60 * 1000 // 2 hours timeout
     }
 
 };
