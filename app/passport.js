17a3a33c46006394efaf9ff1b318ad7a78da3764
Eric Wang <meowster.eric@hotmail.com>
2017-05-19 19:05:21 -0400

    add passport file

---
+++ app/passport.js
@@ -0,0 +1,41 @@
+var passport = require('passport');
+var SamlStrategy = require('passport-saml').Strategy;
+var saml_config = require('configurations/passport_saml_config');
+
+module.exports = function (app) {
+
+    // https://github.com/ritstudentgovernment/passport-saml-example/blob/master/app.js
+    passport.serializeUser(function (user, done) {
+        done(null, user);
+    });
+
+    passport.deserializeUser(function (user, done) {
+        done(null, user);
+    });
+
+    // ----- this part from the web app, select one to use -----
+    // passport.serializeUser(function(user, done) {
+    //     done(null, user._id);
+    // });
+    // passport.deserializeUser(function(id, done) {
+    //     UserModel.findById(id, function(err, user) {
+    //         if (err)
+    //             return done(err, false);
+    //         if(user)
+    //             return done(null, user.toObject());
+    //         else
+    //             return done(new Error("User not found"), false);
+    //     });
+    // });
+
+
+    var samlStrategy = new SamlStrategy(saml_config.saml_data, function (req, profile, done) {
+        console.log(profile);
+        done(new Error('temp stop program for testing'), null);
+    });
+    passport.use(samlStrategy);
+
+    app.use(passport.initialize());
+    app.use(session());
+
+};
