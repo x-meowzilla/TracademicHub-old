1f643a7a03607022ba9b6c488ab19c3657d155a0
Eric Wang <meowster.eric@hotmail.com>
2017-05-25 12:01:27 -0400

    transfer to https, fix relative path issue

--- app/configurations/server_config.js
+++ app/configurations/server_config.js
@@ -2,9 +2,9 @@ module.exports = {
 
     env: 'development',
     port: this.env === 'production' ? 443 : 3000,
-    httpsCert: './certificates/cert.pem', // SP certificate
-    httpsKey: './certificates/key.pem', // SP private key
-    idPCert: './certificates/idp_cert.pem', // IdP public key
+    httpsCert: 'app/certificates/cert.pem', // SP certificate
+    httpsKey: 'app/certificates/key.pem', // SP private key
+    idPCert: 'app/certificates/idp_cert.pem', // IdP public key
     session: {
         key: 'tracademichub.sess', //what the name that the session cookie is stored as
         secret: 'SomeSecret',
