58ef0aa43a55d17de4c8905e491816b045e44885
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-05-29 16:25:56 -0400

    add checking authentication before api calls

--- app/routes/users_api.js
+++ app/routes/users_api.js
@@ -3,46 +3,62 @@ var mongoDB = require('mongodb').MongoClient;
 var dbConfig = require('../configurations/db_config');
 var dbModel = require('../db_models/User');
 
+
+var checkAuthentication = function (req, res, next) {
+    var user = req.session.user;
+
+    if (user) {
+        if (!res.cookie.user) res.cookie('user', user, {secure: true, sameSite: true, httpOnly: true});
+        else next();
+    } else {
+        res.clearCookie('user');
+        res.status(401).send('Please login.').end("Unauthorized");
+    }
+};
+
 // users API
-router.get('/', function (req, res) {
+router.get('/', checkAuthentication, function (req, res) {
     console.log(req.session);
     res.send('GET request accepted.');
 });
 
-router.get('/:id', function (req, res) {
+router.get('/:id', checkAuthentication, function (req, res) {
     res.send('GET request accepted, ID: ' + req.params.id);
 });
 
-router.get('/:id/history', function (req, res) {
+router.get('/:id/history', checkAuthentication, function (req, res) {
     res.send('GET history');
 });
 
-router.post('/', function (req, res) {
+router.post('/', checkAuthentication, function (req, res) {
     res.send('POST request accepted.');
 });
 
-router.delete('/', function (req, res) {
+router.post('/:id/adjustPrivileges', function (req, res) {
+    var userID = req.params.id;
+    dbModel.findByID(userID, function (error, user) {
+        if (error) return res.status(500).end(error);
+
+        if (!user) {
+            return res.status(404).end('User not found.');
+        } else {
+            var accessLevel = Number(req.body.accessLevel);
+            // TODO - adjust user priviledges, try to use promise to avoid callbacks
+        }
+    });
+});
+
+router.delete('/', checkAuthentication, function (req, res) {
     res.send('DELETE!! delete all entries');
 });
 
-router.delete('/:id', function (req, res) {
+router.delete('/:id', checkAuthentication, function (req, res) {
     res.send('DELETE!! delete one entry');
 });
 
 
 // copied from existing code, need to update them
-var checkAuthentication = function (req, res, next) {
-    var user = req.session.user;
-    if (user) {
-        //req.user = user;
-        res.cookie('username', user.username, {secure: true, sameSite: true, httpOnly: true});
-        next();
-    } else {
-        res.clearCookie('username');
-        // res.status(401).end("Unauthorized");
-        res.json(null).end();
-    }
-};
+
 
 var verifyPassword = function (user, password) {
     var hash = crypto.createHmac('sha512', user.salt);
