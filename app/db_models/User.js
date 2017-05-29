2133b7dbc99fb86387af594aea859682b7ad5267
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-05-29 16:34:42 -0400

    comment schema methods

--- app/db_models/User.js
+++ app/db_models/User.js
@@ -5,8 +5,8 @@ var userSchema = new Schema({
 
     username: {type: String, required: true, unique: true},
     password: String, // choose either password or salt+saltedHash
-    // salt: {type: String, required: true},
-    // saltedHash: {type: String, required: true},
+    salt: {type: String, required: true},
+    saltedHash: {type: String, required: true},
     // firstName: {type: String, required: true},
     // lastName: {type: String, required: true},
     // preferredName: {type: String, default: ''},
@@ -16,20 +16,28 @@ var userSchema = new Schema({
     // lastLoginDate: {type: Date, index: true},
     accessLevel: {type: Number, required: true, default: 10}
 
-}, {collection: 'users'});
+}, {collection: 'UsersDB'});
 
 
-// improve these methods!
-userSchema.methods.getUsername = function (done) {
-    var user = this;
-    return done(null, user.username);
-};
+// // improve these methods!
+// userSchema.methods.getUsername = function (done) {
+//     var user = this;
+//     return done(null, user.username);
+// };
+//
+// userSchema.methods.verifyPassword = function (password, done) {
+//     var user = this;
+//
+//     var hash = crypto.createHmac('sha512', user.salt);
+//     hash.update(password);
+//     var digestedPW = hash.digest('base64');
+//     return (user.saltedHash === digestedPW) ? done(null, user) : done(new Error('Incorrect password.'), null);
+// };
+//
+// userSchema.methods.getAccessLevel = function (done) {
+//     var user = this;
+//     return done(null, user.accessLevel);
+// };
 
-userSchema.methods.getAccessLevel = function (done) {
-    var user = this;
-    return done(null, user.accessLevel);
-};
 
-
-// module.exports = mongoose.model('User', userSchema);
-exports.User = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
