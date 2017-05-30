8004bbe3f65f7e0e82e4cb1d83738d6fc0b1ebdd
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-05-29 21:59:14 -0400

    update user schema methods

--- app/db_models/User.js
+++ app/db_models/User.js
@@ -1,12 +1,13 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
 
     username: {type: String, required: true, unique: true},
-    password: String, // choose either password or salt+saltedHash
+    password: String, // choose either password or salt+hash
     salt: {type: String, required: true},
-    saltedHash: {type: String, required: true},
+    hash: {type: String, required: true},
     // firstName: {type: String, required: true},
     // lastName: {type: String, required: true},
     // preferredName: {type: String, default: ''},
@@ -18,26 +19,36 @@ var userSchema = new Schema({
 
 }, {collection: 'UsersDB'});
 
+// encrypt user password and save as salt and hash
+userSchema.pre('save', function (next) {
+    var user = this;
 
-// // improve these methods!
-// userSchema.methods.getUsername = function (done) {
-//     var user = this;
-//     return done(null, user.username);
-// };
-//
-// userSchema.methods.verifyPassword = function (password, done) {
-//     var user = this;
-//
-//     var hash = crypto.createHmac('sha512', user.salt);
-//     hash.update(password);
-//     var digestedPW = hash.digest('base64');
-//     return (user.saltedHash === digestedPW) ? done(null, user) : done(new Error('Incorrect password.'), null);
-// };
-//
-// userSchema.methods.getAccessLevel = function (done) {
-//     var user = this;
-//     return done(null, user.accessLevel);
-// };
+    if (user.isModified('password')) {
+        // create salt and hash to encrypt the password
+        user.salt = crypto.randomBytes(16).toString('base64');
+        user.hash = crypto.createHmac('sha512', user.salt).update(password).digest('base64');
+        user.password = null; // reset the user password to null
+    }
+    // pass to next handler
+    next();
+});
 
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.methods.getUsername = function () {
+    var user = this;
+    return user.username;
+};
+
+userSchema.methods.verifyPassword = function (password) {
+    var user = this;
+    var hash = crypto.createHmac('sha512', user.salt).update(password).digest('base64');
+    return (user.hash === hash);
+};
+
+userSchema.methods.getAccessLevel = function () {
+    var user = this;
+    return user.accessLevel;
+};
+
+var UsersDB = mongoose.model('User', userSchema);
+module.exports = UsersDB;
