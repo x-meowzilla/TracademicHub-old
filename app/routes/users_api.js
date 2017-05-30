940634d47abc6ed656e11c13d488c0be52f1be0f
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-05-29 22:06:04 -0400

    clean up user api

--- app/routes/users_api.js
+++ app/routes/users_api.js
@@ -1,8 +1,5 @@
 var router = require('express').Router();
-var mongoDB = require('mongodb').MongoClient;
-var dbConfig = require('../configurations/db_config');
 var dbModel = require('../db_models/User');
-
 
 var checkAuthentication = function (req, res, next) {
     var user = req.session.user;
@@ -55,74 +52,6 @@ router.delete('/', checkAuthentication, function (req, res) {
 router.delete('/:id', checkAuthentication, function (req, res) {
     res.send('DELETE!! delete one entry');
 });
-
-
-// copied from existing code, need to update them
-
-
-var verifyPassword = function (user, password) {
-    var hash = crypto.createHmac('sha512', user.salt);
-    hash.update(password);
-    var value = hash.digest('base64');
-    return (user.saltedHash === value);
-};
-
-var mongoAction = function (tableName, action, data, callback) {
-    mongoDB.connect(dbConfig.dbURL, function (err, db) {
-        if (err) {
-            return console.log(err);
-        } else {
-            console.log("Mongo database connected");
-            var collection = db.collection(tableName);
-            switch (action) {
-                case 'insertOne':
-                    collection.insertOne(data.doc, data.options, callback);
-                    break;
-                case 'find':
-                    collection.find(data.query).toArray(callback);
-                    break;
-                case 'findOne':
-                    collection.findOne(data.query, data.options, callback);
-                    break;
-                case 'updateOne':
-                    collection.updateOne(data.query, data.doc, data.options, callback);
-                    break;
-                case 'deleteMany':
-                    collection.deleteMany(data.query, data.options, callback);
-                    break;
-                case 'deleteOne':
-                    collection.deleteOne(data.query, data.options, callback);
-                    break;
-                case 'drop':
-                    collection.drop(data.options, callback);
-                    break;
-            }
-        }
-        db.close();
-    })
-};
-
-// testing schema
-// mongoDB.connect(dbConfig.dbURL, function (err, db) {
-//     var collection = db.collection('users');
-//     collection.insertOne({firstname: 'eric'}, function (err, result) {
-//         if (err) console.log(err);
-//         else console.log(result.ops);
-//     });
-//     db.close();
-// });
-
-
-// var user = new dbModel.User();
-// var promise = user.save();
-// promise.then(function (doc) {
-//     console.log(doc);
-// });
-
-
-// mongoAction('users', 'drop', {options: {}}, function (err, user) {
-//     console.log("drop users table");
-// });
 
 
 module.exports = router;
