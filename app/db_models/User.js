867c9deb3dc223ae235222a4ce1a230059a1c6ca
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-05-31 23:35:09 -0400

    adding more methods to user schema

--- app/db_models/User.js
+++ app/db_models/User.js
@@ -10,30 +10,16 @@ var userSchema = new Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     preferredName: {type: String, default: ''},
-    // utorid: {type: String, required: true},
+    accessLevel: {type: Number, required: true, default: 10}
+    // lastLoginDate: {type: Date, index: true}
     // studentNumber: {type: String, unique: true},
     // email: {type: String},
-    // // libraryNumber: {type: String, unique: true}, //
-    // lastLoginDate: {type: Date, index: true},
-    accessLevel: {type: Number, required: true, default: 10}
+    // libraryNumber: {type: String, unique: true}, //
+
 
 }, {collection: 'UsersDB'});
 
-// encrypt user password and save as salt and hash
-//userSchema.pre('save', function (next) {
-//    var user = this;
-//
-//    if (user.isModified('password')) {
-//        // create salt and hash to encrypt the password
-//        user.salt = crypto.randomBytes(16).toString('base64');
-//        user.hash = crypto.createHmac('sha512', user.salt).update(password).digest('base64');
-//        user.password = null; // reset the user password to null
-//    }
-//    // pass to next handler
-//    next();
-//});
-
-
+// method for local user
 userSchema.methods.encryptPassword = function (password) {
     var user = this;
     var salt = crypto.randomBytes(16).toString('base64');
@@ -42,21 +28,43 @@ userSchema.methods.encryptPassword = function (password) {
     user.hash = hash;
 };
 
-userSchema.methods.getUsername = function () {
-    var user = this;
-    return user.username;
-};
-
+// method for local user
 userSchema.methods.verifyPassword = function (password) {
     var user = this;
     var hash = crypto.createHmac('sha512', user.salt).update(password).digest('base64');
     return (user.hash === hash);
 };
 
+userSchema.methods.getUsername = function () {
+    var user = this;
+    return user.username;
+};
+
+userSchema.methods.getFullName = function () {
+    var user = this;
+    return user.firstName + ' ' + user.lastName;
+};
+
+userSchema.methods.getPreferredName = function () {
+    var user = this;
+    return user.preferredName;
+};
+
+userSchema.methods.setPreferredName = function (preferredName) {
+    var user = this;
+    user.preferredName = preferredName;
+};
+
 userSchema.methods.getAccessLevel = function () {
     var user = this;
     return user.accessLevel;
 };
+
+userSchema.methods.setAccessLevel = function (accessLevel) {
+    var user = this;
+    user.accessLevel = accessLevel;
+};
+
 
 var UserModel = mongoose.model('User', userSchema);
 module.exports = UserModel;
