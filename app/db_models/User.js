8e695383d0395efb6de97b372e8364778c9168fb
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-06-01 20:48:34 -0400

    Revert "Revert "schema improvement""
    This reverts commit d7081c7b1b5c02a262784bbd90adc8a79a588c5e.

--- app/db_models/User.js
+++ app/db_models/User.js
@@ -7,17 +7,21 @@ var userSchema = new Schema({
 
     utorid: {type: String, required: true, unique: true}, // required!!
     email: {type: String, unique: true},
-    salt: {type: String, unique: true},
-    hash: {type: String},
-    firstName: {type: String, default: ''},
-    lastName: {type: String, default: ''},
-    preferredName: {type: String, default: ''},
+    password: {
+        isLocalUser: {type: Boolean, required: true, default: true},
+        salt: {type: String, unique: true},
+        hash: {type: String}
+    },
+    name: {
+        firstName: {type: String, default: ''},
+        lastName: {type: String, default: ''},
+        preferredName: {type: String, default: ''}
+    },
     accessLevel: {type: Number, required: true, default: access.ACCESS_LEVEL_STUDENT}
 
     // lastLoginDate: {type: Date, index: true}
     // studentNumber: {type: String, unique: true},
     // libraryNumber: {type: String, unique: true}, //
-
 
 }, {collection: 'UsersDB'});
 
@@ -26,15 +30,15 @@ userSchema.methods.encryptPassword = function (password) {
     var user = this;
     var salt = crypto.randomBytes(16).toString('base64');
     var hash = crypto.createHmac('sha512', salt).update(password).digest('base64');
-    user.salt = salt;
-    user.hash = hash;
+    user.password.salt = salt;
+    user.password.hash = hash;
 };
 
 // method for local user
 userSchema.methods.verifyPassword = function (password) {
     var user = this;
-    var hash = crypto.createHmac('sha512', user.salt).update(password).digest('base64');
-    return (user.hash === hash);
+    var hash = crypto.createHmac('sha512', user.password.salt).update(password).digest('base64');
+    return (user.password.hash === hash);
 };
 
 userSchema.methods.getUTORid = function () {
@@ -44,17 +48,23 @@ userSchema.methods.getUTORid = function () {
 
 userSchema.methods.getFullName = function () {
     var user = this;
-    return user.firstName + ' ' + user.lastName;
+    return user.name.firstName + ' ' + user.name.lastName;
+};
+
+userSchema.methods.setName = function (firstName, lastName) {
+    var user = this;
+    user.name.firstName = firstName;
+    user.name.lastName = lastName;
 };
 
 userSchema.methods.getPreferredName = function () {
     var user = this;
-    return user.preferredName;
+    return user.name.preferredName;
 };
 
 userSchema.methods.setPreferredName = function (preferredName) {
     var user = this;
-    user.preferredName = preferredName;
+    user.name.preferredName = preferredName;
 };
 
 userSchema.methods.getAccessLevel = function () {
