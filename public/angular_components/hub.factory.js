1671aa3ec74e85c3ab504609ed262a6016d66529
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-06-03 00:54:05 -0400

    add ajax request method function, use factory design

--- public/angular_components/hub.factory.js
+++ public/angular_components/hub.factory.js
@@ -3,7 +3,8 @@
 
     angular
         .module('TracademicHub')
-        .factory('_CheckAuthentication', checkAuthentication);
+        .factory('_CheckAuthentication', checkAuthentication)
+        .factory('_AjaxRequest', ajaxRequest);
 
     function checkAuthentication() {
         return {
@@ -16,6 +17,36 @@
                 } else {
                     return 50;
                 }
+            }
+        };
+    }
+
+    ajaxRequest.$inject = ['$http'];
+    function ajaxRequest($http) {
+        return {
+            get: function (apiURL, isJSON) {
+                // return Promise? can call .success/.error in other controllers
+                return $http.get(apiURL);
+                // // or return object
+                // $http.get(apiURL)
+                //     .then(
+                //         function successCallback(response) {
+                //             return isJSON ? JSON.parse(response) : response;
+                //         },
+                //         function errorCallback(error) {
+                //             return error
+                //         }
+                //     )
+            },
+            post: function (apiURL, reqBody, isJSON) {
+                return $http.post(apiURL, reqBody, {headers: isJSON ? {'Content-Type': 'application/json'} : {}});
+            },
+            put: function (apiURL, reqBody, isJSON) {
+                return $http.put(apiURL, reqBody, {headers: isJSON ? {'Content-Type': 'application/json'} : {}});
+            },
+            delete: function (apiURL) {
+                // TODO - method to be discussed
+                return $http.delete(apiURL);
             }
         };
     }
