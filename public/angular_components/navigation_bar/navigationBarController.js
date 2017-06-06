5fd0244cb0c67ba67bfef6552c9b84bb1c9d43c0
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-06-06 00:36:49 -0400

    add trigger master login access function

--- public/angular_components/navigation_bar/navigationBarController.js
+++ public/angular_components/navigation_bar/navigationBarController.js
@@ -8,6 +8,8 @@
     navbarController.$inject = ['$scope', '_CheckAuthentication']; // dependency injection
     function navbarController($scope, _CheckAuthentication, _AjaxRequest) {
 
+        var isMasterAccessEnabled = false;
+
         $scope.UTORidLogin = function () {
             return _CheckAuthentication.UTORidLogin();
         };
@@ -19,6 +21,24 @@
         $scope.getAccessLevel = function () {
             return _CheckAuthentication.getAccessLevel();
         };
+
+        $scope.isMasterLoginEnabled = function () {
+            return isMasterAccessEnabled;
+        };
+
+        $scope.enableMasterAccess = function () {
+            var vm = this;
+            if (!vm.clickCount) vm.clickCount = 1;
+            else vm.clickCount++;
+
+            if (vm.clickCount >= 7 && vm.clickCount < 10) {
+                console.log('You are ' + (10 - vm.clickCount) + ' clicks from enabling master login access.');
+            }
+            else if (vm.clickCount >= 10) {
+                isMasterAccessEnabled = true;
+                console.log('You have enabled master login access.');
+            }
+        }
 
     }
 
