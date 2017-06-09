6ba332d24ce6eb66378d9285978a24e296be96e4
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-06-09 16:20:51 -0400

    split utorid authentication into a single dependency

--- public/angular_components/hub.factory.js
+++ public/angular_components/hub.factory.js
@@ -3,24 +3,39 @@
 
     angular
         .module('TracademicHub')
+        .factory('_UTORidAuthentication', utoridAuthentication)
         .factory('_CheckAuthentication', checkAuthentication)
         .factory('_AjaxRequest', ajaxRequest);
 
-    function checkAuthentication() {
+    function utoridAuthentication() {
         return {
             UTORidLogin: function () {
                 // we need to use $http service to call our api
                 window.location = "/Shibboleth.sso/Login";
-            },
+            }
+        };
+    }
+
+    function checkAuthentication() {
+        return {
+
+            _isAuthenticated: false,
+            _accessLevel: -1,
+            _displayName: "",
+
             isAuthenticated: function () {
-                return false;
+                return this._isAuthenticated;
             },
             getAccessLevel: function () {
-                if (!this.isAuthenticated()) {
-                    return -1;
-                } else {
-                    return 50;
-                }
+                return this._accessLevel;
+            },
+            getDisplayName: function () {
+                return this._displayName;
+            },
+            logout: function () {
+                this._isAuthenticated = false;
+                this._accessLevel = -1;
+                this._displayName = "";
             }
         };
     }
