c0c0d7b25461cfe05b886d0d20f7101a861ef683
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-06-12 12:00:42 -0400

    fix a typo

--- app/modules/access_level.js
+++ app/modules/access_level.js
@@ -8,7 +8,7 @@ module.exports.ACCESS_LEVEL_STUDENT_DESCRIPTION = 'Student';
 // module.exports.ACCESS_LEVEL_KIOSK_DESCRIPTION = 'Kiosk';
 
 module.exports.ACCESS_LEVEL_TA = 20;
-module.exports.ACCESS_LEVEL_TA_DESCRIPTION = 'Description';
+module.exports.ACCESS_LEVEL_TA_DESCRIPTION = 'TA';
 
 module.exports.ACCESS_LEVEL_INSTRUCTOR = 30;
 module.exports.ACCESS_LEVEL_INSTRUCTOR_DESCRIPTION = 'Instructor';
