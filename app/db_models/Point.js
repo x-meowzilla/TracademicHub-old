985d5ded053ae5f740e3bcdb6b9a0e7ccc751da0
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-06-13 12:48:14 -0400

    add category to point schema

--- app/db_models/Point.js
+++ app/db_models/Point.js
@@ -12,9 +12,10 @@ var pointSchema = new Schema({
         name: {type: String, required: true}
     },
     grantDate: {type: Date, default: Date.now, required: true},
-    value: {type: Number, required: true}
+    value: {type: Number, default: 1, required: true},
+    category: {type: Schema.Types.ObjectId, ref: 'PointCategory', required: true}
 
-}, {collection: 'PointsReference'});
+}, {collection: 'PointsDB'});
 
 
 var PointModel = mongoose.model('Point', pointSchema);
