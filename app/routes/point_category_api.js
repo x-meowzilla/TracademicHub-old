3009cd892c70dd9e3843c67fa3275747bb0bb2ea
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-06-14 01:36:12 -0400

    add points category apis

---
+++ app/routes/point_category_api.js
@@ -0,0 +1,47 @@
+var router = require('express').Router();
+var PointCategoryModel = require('../db_models/PointCategory');
+
+// point category URI: .../api/points-category/
+router.get('/', function (req, res) {
+    PointCategoryModel.find({})
+        .then(function (categoryArray) {
+            res.json(categoryArray).end();
+        })
+        .catch(function (error) {
+            res.status(500).end(error.errmsg);
+        });
+});
+
+router.put('/', function (req, res) {
+    PointCategoryModel.findOne({name: req.body.categoryName})
+        .then(function (category) {
+            if (category) {
+                res.status(409).end('Category: \'' + category.name + '\' exists.');
+            } else {
+                category = new PointCategoryModel({name: req.body.categoryName});
+                category.save()
+                    .then(function (category) {
+                        res.json(category).end();
+                    })
+                    .catch(function (error) {
+                        res.status(500).end(error.errmsg);
+                    });
+            }
+        })
+        .catch(function (error) {
+            res.status(500).end(error.errmsg);
+        });
+});
+
+router.delete('/:name', function (req, res) {
+    PointCategoryModel.remove({name: req.params.name})
+        .then(function () {
+            res.send('Category: \'' + req.params.name + '\' deleted.').end();
+        })
+        .catch(function (error) {
+            res.status(500).end(error.errmsg);
+        })
+});
+
+
+module.exports = router;
