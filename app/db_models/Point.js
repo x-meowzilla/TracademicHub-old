5fb5c15c81b32ccec854e3019c7f6cc1df08e729
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-06-19 14:45:10 -0400

    points api ready

--- app/db_models/Point.js
+++ app/db_models/Point.js
@@ -5,11 +5,19 @@ var pointSchema = new Schema({
 
     assigner: {
         id: {type: Schema.Types.ObjectId, ref: 'User', required: true},
-        name: {type: String, required: true}
+        name: {
+            firstName: {type: String, required: true},
+            lastName: {type: String, required: true},
+            preferredName: {type: String, required: true}
+        }
     },
     assignee: {
         id: {type: Schema.Types.ObjectId, ref: 'User', required: true},
-        name: {type: String, required: true}
+        name: {
+            firstName: {type: String, required: true},
+            lastName: {type: String, required: true},
+            preferredName: {type: String, required: true}
+        }
     },
     grantDate: {type: Date, default: Date.now, required: true},
     value: {type: Number, default: 1, required: true},
@@ -17,6 +25,25 @@ var pointSchema = new Schema({
 
 }, {collection: 'PointsCollection'});
 
+pointSchema.statics.getAllHistory = function () {
+    var point = this.model('Point');
+    return point.find({});
+};
+
+pointSchema.statics.getHistoryByAssignerID = function (assignerID) {
+    var point = this.model('Point');
+    return point.find({assigner: {id: assignerID}});
+};
+
+pointSchema.statics.getHistoryByAssigneeID = function (assigneeID) {
+    var point = this.model('Point');
+    return point.find({assignee: {id: assigneeID}});
+};
+
+pointSchema.statics.deleteByID = function (pointID) {
+    var point = this.model('Point');
+    return point.remove({_id: pointID});
+};
 
 var PointModel = mongoose.model('Point', pointSchema);
 module.exports = PointModel;
