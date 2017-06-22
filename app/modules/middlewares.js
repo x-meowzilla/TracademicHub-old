8ce6cbd96027a7885ceff2e48ec5334fb7054580
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-06-22 14:07:45 -0400

    change a status code

--- app/modules/middlewares.js
+++ app/modules/middlewares.js
@@ -2,7 +2,7 @@ module.exports.checkAuthentication = function (req, res, next) {
     "use strict";
 
     if (!req.isAuthenticated())
-        return res.status(403).send('Please login before performing this action.').end('Forbidden');
+        return res.status(401).send('Please login before performing this action.').end('Unauthorized');
     else
         return next();
 };
