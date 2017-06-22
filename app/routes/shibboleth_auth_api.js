c6c2322134d1d3fa07d50e9a4be5936e6cef4718
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-06-22 00:58:20 -0400

    enable session to shibboleth login, reimplement login based on new logic

--- app/routes/shibboleth_auth_api.js
+++ app/routes/shibboleth_auth_api.js
@@ -1,26 +1,28 @@
 var router = require('express').Router();
 var passport = require('passport');
-
+var util = require('../modules/utility');
 
 router.get('/Login', function (req, res) {
-    passport.authenticate('saml',
-        {
+    passport.authenticate('saml', {
             successRedirect: '/', // welcome page - can change to user management dashboard page
-            failureRedirect: '/Shibboleth.sso/Login'
+            failureRedirect: '/Shibboleth.sso/Login',
+            session: true
         },
         function (error, user) { // user guaranteed exists! check passport_authentication.js
-            if (error) return res.status(500).end(error.errmsg);
-
-            // set data to session and cookie
-            req.session.user = user;
-            res.cookie('userID', user._id);
-
+            if (error) {
+                return res.status(500).end(error.errmsg);
+            } else {
+                req.login(user, function () {
+                    return res.json(util.retrieveBasicUserData(user)).end();
+                });
+            }
             // if this user data has not been imported (name field exists), then redirect to update info page
-            if (!user.name) res.redirect('/some-page-to-update-basic-user-info');
-            return res.status(200).json(user).end();
+            // if (!user.name) res.redirect('/some-page-to-update-basic-user-info');
+            // return res.status(200).json(user).end();
         })(req, res);
 });
 
+// check this!
 router.post('/SAML2/POST',
     passport.authenticate('saml',
         {
