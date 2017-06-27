8cec2ff3839a5af5516f10120ee62cb8f65db39d
zhoubecky <xiaoh.zhou@mail.utoronto.ca>
2017-06-27 09:04:16 -0400

    finally finish rewriting datatable

--- public/angular_components/userProfile/statesController.js
+++ public/angular_components/userProfile/statesController.js
@@ -6,9 +6,9 @@
         .controller('statesController', statesController)
         .directive('sortRecords', sortRecords);
 
-    statesController.$inject = ['$scope', '_CheckAuthentication', '_AjaxRequest']; // dependency injection
+    statesController.$inject = ['$scope', '_CheckAuthentication', '_AjaxRequest', '$filter']; // dependency injection
 
-    function statesController($scope, _CheckAuthentication, _AjaxRequest) {
+    function statesController($scope, _CheckAuthentication, _AjaxRequest, $filter) {
         $scope.isAuthenticated = function () {
             return _CheckAuthentication.isAuthenticated();
         };
@@ -29,25 +29,91 @@
                 )
         }());
 
+
+        $scope.viewby = '5';
+
         $scope.sort = {
             sortingOrder : '',
             reverse : false
         };
 
-        $scope.pagedItems = [];
-
         $scope.items = [
             {"fullName":1,"preferredName":"name 1","category":"description 1","course":"field3 1","date":"field4 1"},
             {"fullName":2,"preferredName":"name 2","category":"description 1","course":"field3 5","date":"field4 2"},
             {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
-            {"fullName":4,"preferredName":"name 4","category":"description 1","course":"field3 2","date":"field4 4"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
+            {"fullName":3,"preferredName":"name 3","category":"description 1","course":"field3 3","date":"field4 3"},
             {"fullName":5,"preferredName":"name 5","category":"description 1","course":"field3 7","date":"field4 5"},
-            {"fullName":6,"preferredName":"name 6","category":"description 1","course":"field3 6","date":"field4 6"}
+            {"fullName":6,"preferredName":"name 6","category":"description 1","course":"field3 6","date":"field4 6"},
+            {"fullName":9,"preferredName":"name 6","category":"description 1","course":"field3 6","date":"field4 6"}
         ];
 
 
-        $scope.searchRecord = '';     // set the default search/filter term
+        $scope.pager = {
+            currentPage: 1,
+            gap: 5
+        }
+        
+        $scope.getTotalPagesNum = function () {
+            return Math.ceil($filter('filter')($scope.items, $scope.searchRecord).length/$scope.viewby);
+        }
 
+        $scope.range = function (start, gap) {
+            var end =  start + gap;
+            var size = $scope.getTotalPagesNum();
+            var ret = [];
+
+            if (size < end) {
+                end = size + 1;
+                start = size > gap ? size - $scope.pager.gap : 1;
+            }
+            for (var i = start; i < end; i++) {
+                ret.push(i);
+            }
+            return ret;
+        };
+
+        $scope.setPage = function (page) {
+            if (page < 1 || page > $scope.getTotalPagesNum()) {
+                return;
+            }
+
+            // get pager object
+            $scope.pager.currentPage = page;
+
+            var startIdx = (page - 1) * $scope.viewby - 1;
+            var endIdx = page * $scope.viewby;
+        }
     };
 
     function sortRecords() {
@@ -59,10 +125,10 @@
                 sort: '='
             },
             template :
-            ' <span ng-click="sort_by(order)">'+
+            ' <i ng-click="sort_by(order)">'+
             '    <span ng-transclude></span>'+
             '    <i ng-class="selectedCls(order)"></i>'+
-            '</span>',
+            '</i>',
             link: function(scope) {
                 // change sorting order
                 scope.sort_by = function(newSortingOrder) {
