285de69d5f505df2289a5f536de6300687a8a3ec
zhoubecky <xiaoh.zhou@mail.utoronto.ca>
2017-07-04 01:09:41 -0400

    add more demo data

--- public/angular_components/userProfile/userManagementHistoryController.js
+++ public/angular_components/userProfile/userManagementHistoryController.js
@@ -18,21 +18,25 @@
         };
 
         $scope.items = [
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
-            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"}
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva1","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva2","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"TA", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva3","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"delete", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva4","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"modify access privilage", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva5","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"modify user info", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva6","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"gain points", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva7","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"gain points", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva8","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"gain points", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva9","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"TA", "operationType":"assign points", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva19","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"TA", "operationType":"assign points", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva18","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"TA", "operationType":"assign points", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva0","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"TA", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva11","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"TA", "operationType":"delete", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva12","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva13","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva14","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva15","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva16","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"},
+            {"name":{"preferredName":"asdfasd123123f","firstName":"asfva17","lastName":"jgdf"}, "email":"abc@gmail.com", "accessPrivilege":"Student", "operationType":"add", "operationTime":"2017-06-29T17:07:04.837Z"}
         ];
 
         (function () {
