ba631f8d33f8e48b49795c78a23b68363289ec3d
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-07-05 09:25:23 -0400

    add access privilege api

--- app/db_models/AccessPrivilege.js
+++ app/db_models/AccessPrivilege.js
@@ -10,14 +10,28 @@ var privilegeSchema = new Schema({
 
 
 privilegeSchema.statics.findByPrivilegeValueAndDescription = function (accessValue, description) {
+    "use strict";
     var access = this.model('AccessPrivilege');
     return access.findOne({value: accessValue, description: description});
 };
 
 privilegeSchema.statics.findByIds = function (reqUserAccessID, targetUserAccessID) {
+    "use strict";
     // this method will return 2 access privilege objects to compare the value
     var access = this.model('AccessPrivilege');
     return access.find({_id: {$in: [mongoose.Types.ObjectId(reqUserAccessID), mongoose.Types.ObjectId(targetUserAccessID)]}});
+};
+
+privilegeSchema.statics.findAccessPrivilegeData = function (findDoc) {
+    "use strict";
+    var access = this.model('AccessPrivilege');
+    return access.find(findDoc);
+};
+
+privilegeSchema.statics.deleteAccessPrivilegeData = function (deleteDoc) {
+    "use strict";
+    var access = this.model('AccessPrivilege');
+    return access.remove(deleteDoc);
 };
 
 var privilegeModel = mongoose.model('AccessPrivilege', privilegeSchema);
---
+++ app/routes/privilege_api.js
@@ -0,0 +1,62 @@
+var router = require('express').Router();
+var mw = require('../modules/middlewares');
+var PrivilegeModel = require('../db_models/AccessPrivilege');
+
+// point URI: .../api/privilege/
+router.get('/', mw.checkAuthentication, mw.haveMinimumInstructorAccessPrivilege, function (req, res) {
+    "use strict";
+    var findDoc = {};
+    for (var arg in req.query) {
+        switch (arg) {
+            case '_id':
+            case 'value':
+            case 'description':
+                findDoc[arg] = req.query[arg];
+                break;
+        }
+    }
+
+    PrivilegeModel.findAccessPrivilegeData(findDoc)
+        .then(function (pointsArray) {
+            console.log(pointsArray);
+            return res.json(pointsArray).end();
+        })
+        .catch(function (error) {
+            return res.status(500).send(error).end();
+        });
+});
+
+router.delete('/', mw.checkAuthentication, mw.haveMinimumInstructorAccessPrivilege, function (req, res) {
+    "use strict";
+    var deleteDoc = {};
+    for (var arg in req.query) {
+        switch (arg) {
+            case '_id':
+            case 'value':
+            case 'description':
+                deleteDoc[arg] = req.query[arg];
+                break;
+        }
+    }
+
+    if (Object.keys(deleteDoc).length === 0) { // strictly check here. Valid query string must present!
+        return res.status(400).send('Failed to delete. Delete option not found.').end();
+    } else {
+        PrivilegeModel.deleteAccessPrivilegeData(deleteDoc)
+            .then(function (result) {
+                return result.result.n;
+            })
+            .then(function (numRecords) {
+                return numRecords ? 'Delete succeeded. ' + result.result.n + ' records deleted.' : 'Attention: No record has been deleted.';
+            })
+            .then(function (msg) {
+                return res.send(msg).end();
+            })
+            .catch(function (error) {
+                return res.status(500).send(error).end();
+            });
+    }
+});
+
+
+module.exports = router;
