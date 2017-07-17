65f5fa118ba03ee3f92bab134ea6625634f0801f
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-07-17 17:33:30 -0400

    add passport config

--- app/configurations/passport_config.js
+++ app/configurations/passport_config.js
@@ -1,19 +1,20 @@
 var fs = require('fs');
-var serverConfig = require('./server_config');
 
 module.exports = {
 
+    SPCert: 'app/certificates/sp-cert.pem', // SP certificate
+    SPKey: 'app/certificates/sp-key.pem', // SP private key
     samlData: {
         // URL that goes from the Service Provider -> Identity Provider
         entryPoint: 'https://idp.utorauth.utoronto.ca/idp/profile/SAML2/Redirect/SSO',
         // URL that goes from the Identity Provider -> Service Provider
-        callbackUrl: '',
-        // Usually specified as `/shibboleth` from site root
-        issuer: 'https://idp.utorauth.utoronto.ca/shibboleth',
-        // Service Provider private key
-        // decryptionPvk: fs.readFileSync(serverConfig.httpsKey, 'utf8'),
+        callbackUrl: 'https://localhost:3000/',
+        // Usually specified as `/shibboleth` from site root. Note: this must be identical with entity ID in shibboleth server configuration
+        issuer: 'https://sp.tracademic.utsc.utoronto.ca/shibboleth',
         // Service Provider Certificate
-        // privateCert: fs.readFileSync(serverConfig.httpsCert, 'utf8'),
+        privateCert: fs.readFileSync(this.SPCert, 'utf8'),
+        // Service Provider private key
+        decryptionPvk: fs.readFileSync(this.SPKey, 'utf8'),
         // Identity Provider's public key
         // cert: fs.readFileSync(serverConfig.idPCert, 'utf8'),
         identifierFormat: null,
