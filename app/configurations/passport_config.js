93dadd7666027a15e0b487f9a9493db52e66b82a
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-07-20 00:01:35 -0400

    clean up

--- app/configurations/passport_config.js
+++ app/configurations/passport_config.js
@@ -11,18 +11,14 @@ module.exports = {
         // callbackUrl: 'https://localhost:3000/', // possibly not needed as we already set in shibboleth API
         // Usually specified as `/shibboleth` from site root. Note: this must be identical with entity ID in shibboleth server configuration
         issuer: 'https://tracademic.utsc.utoronto.ca/shibboleth',
-        identifierFormat: null,
-        // Service Provider Certificate
-	disableRequestedAuthnContext: true,
-        acceptedClockSkewMs: 5000,
-	decryptionPvk: "saml-decrypt.pem", //The private key used to decrypt SAML responses.
-	cert: "saml-verify.crt"//The certificate used to verify SAML responses. Omitting this will allow user spoofing!
-	// Service Provider private key
+        // Service Provider private key
+        decryptionPvk: fs.readFileSync('app/certificates/saml-decrypt.pem', 'utf8'), //The private key used to decrypt SAML responses.
         // Identity Provider's public key
-        // cert: fs.readFileSync(serverConfig.idPCert, 'utf8'),
-        //validateInResponseTo: false,
-        //disableRequestedAuthnContext: true,
-        //acceptedClockSkewMs: 5000
+        cert: fs.readFileSync('app/certificates/saml-verify.crt', 'utf8'), //The certificate used to verify SAML responses. Omitting this will allow user spoofing!
+        identifierFormat: null,
+        disableRequestedAuthnContext: true,
+        // validateInResponseTo: true,
+        acceptedClockSkewMs: 5000
     }
 
 };
--- app/configurations/server_config.js
+++ app/configurations/server_config.js
@@ -1,17 +1,12 @@
 module.exports = {
 
     port: process.env.PORT || 8000,
-//    httpsCert: 'app/certificates/cert.pem', // SP certificate
-//    httpsKey: 'app/certificates/key.pem', // SP private key
     httpsCert: "/etc/ssl/certs/tracademic.crt",
     httpsKey: "/etc/ssl/private/tracademic.key",
     httpsCa: "/etc/ssl/certs/tracademic.ca-bundle",
-
-    // idPCert: 'certificates/idp_cert.pem', // IdP public key
     session: {
-//        key: 'tracademichub.sess', //what the name that the session cookie is stored as
-        key: 'points.sess', //what the name that the session cookie is stored as
-        secret: 'wqcVwJ6rNgrpEpZBppGt', //DO NOT LEAK
+        key: 'tracademichub.sess', // what the name that the session cookie is stored as
+        secret: 'mwx2NOvSeJ4MJEyGfs5d', // DO NOT LEAK
         timeout: new Date(Date.now() + 2 * 60 * 60 * 1000) // 2 hours timeout
     }
 
