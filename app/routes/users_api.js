73ea5833dab86ba51525ef2b84ab1d6b00b30531
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-08-08 18:21:54 -0400

    finally students can enrol in the course now.

--- app/routes/users_api.js
+++ app/routes/users_api.js
@@ -6,7 +6,7 @@ var uploadLocal = multer({dest: 'app/user_uploads/', limits: {files: 1, fileSize
 var mw = require('../modules/middlewares');
 var util = require('../modules/utility');
 var UserModel = require('../db_models/User');
-// var PrivilegeModel = require('../db_models/AccessPrivilege');
+var CourseModel = require('../db_models/Course');
 
 // users URI: .../api/users/
 router.get('/', function (req, res) {
@@ -18,9 +18,11 @@ router.get('/', function (req, res) {
             case 'utorid':
             case 'email':
             case 'studentNumber':
-            case 'accessPrivilege':
             case 'isActive':
                 findDoc[arg] = req.query[arg];
+                break;
+            case 'courseEnrolled':
+                findDoc[arg + '.course'] = req.query[arg];
                 break;
             case 'firstName':
             case 'lastName':
@@ -43,101 +45,104 @@ router.get('/', function (req, res) {
 
 router.post('/', uploadMemory.single('csvfile'), function (req, res) {
     "use strict";
-
-    console.log('-------------------------');
-
-    console.log(req.file.buffer.toString());
-
-    res.end();
-
-
-    // // read csv file content from buffer and split file header and content, then generate file header and its corresponding indices
-    // var csv = processFileData(req.file.buffer.toString());
-    // var header = generateFileHeader(csv.header);
-    // // if the header is invalid, return status 400 and error message
-    // if (!isFileHeaderProperlyFormatted(header))
-    //     return res.status(400).send('Student CSV file is not properly formatted.').end();
+    // read csv file content from buffer and split file header and content, then generate file header and its corresponding indices
+    var csv = parseCSVFile(req.file.buffer.toString());
+    csv.header = parseCSVHeaders(csv.header);
+    // if the header is invalid, return status 400 and error message
+    if (!isValidCSVHeaders(csv.header))
+        return res.status(400).send('Student CSV file is not properly formatted.').end();
     // // if the header is valid, then generate user data object
-    // var userDataArray = generateUserData(header, csv.content);
-    //
+    var userDataArray = parseUserData(csv.header, csv.content);
 
-    // PrivilegeModel.findAccessPrivilegeData({value: util.ACCESS_STUDENT, description: util.ACCESS_STUDENT_DESCRIPTION})
-    //     .then(function (accessArray) { // map student access to each user data object
-    //         return userDataArray.map(function (userData) {
-    //             userData.accessPrivilege = accessArray[0]._id;
-    //             return userData;
-    //         });
-    //     })
-    //     .then(function (userDataArray) { // save user, if duplicate exists, ignore it.
-    //         var result = [];
-    //         userDataArray.forEach(function (userData) {
-    //             result.push(new UserModel(userData).save()
-    //                 .then(function (user) {
-    //                     return true;
-    //                 })
-    //                 .catch(function (error) {
-    //                     return false;
-    //                 })
-    //             );
-    //         });
-    //         return Promise.all(result); // this solution returns an array of boolean indicating save new or get existing records to database
-    //     })
-    //     .then(function (resultArray) {
-    //         var total = resultArray.length;
-    //         var newRecord = resultArray.reduce(function (sum, boolResult) {
-    //             return sum + boolResult;
-    //         }, 0);
-    //         var oldRecord = total - newRecord;
-    //         var response = 'Imported from student CSV file. Total ' + total + ' records found: ';
-    //         response += newRecord ? newRecord + ' new student records saved successfully. ' : '';
-    //         response += oldRecord ? oldRecord + ' existing student records remain unchanged.' : '';
-    //         return res.send(response).end();
-    //     })
-    //     .catch(function (error) {
-    //         return res.status(500).send(error.message).end();
-    //     });
+    CourseModel.findCourseData({_id: req.body.course})
+        .then(function (courseArray) {
+            return {course: courseArray[0]._id, privilege: _privilegeFilter(courseArray[0].userPrivileges, 'Student')};
 
-    // function processFileData(csvString) {
-    //     var contentArray = csvString.split('\n').map(function (line) {
-    //         return line.trim('\r').split(',');
-    //     });
-    //     var headerArray = contentArray.shift().map(function (header) {
-    //         return header.toLowerCase();
-    //     });
-    //     return {header: headerArray, content: contentArray};
-    // }
-    //
-    // function generateFileHeader(headerArray) {
-    //     // find the indices for each field, build user data object and push to array
-    //     var utoridIdx = headerArray.indexOf('UTORiD'.toLowerCase());
-    //     var emailIdx = headerArray.indexOf('Email'.toLowerCase());
-    //     var studentNumberIdx = Math.max(headerArray.indexOf('Student Number'.toLowerCase()), headerArray.indexOf('StudentNumber'.toLowerCase()));
-    //     var firstNameIdx = Math.max(headerArray.indexOf('First Name'.toLowerCase()), headerArray.indexOf('FirstName'.toLowerCase()));
-    //     var lastNameIdx = Math.max(headerArray.indexOf('Last Name'.toLowerCase()), headerArray.indexOf('LastName'.toLowerCase()));
-    //     return {utorid: utoridIdx, email: emailIdx, studentNumber: studentNumberIdx, firstName: firstNameIdx, lastName: lastNameIdx};
-    // }
-    //
-    // function isFileHeaderProperlyFormatted(header) {
-    //     return Object.keys(header).reduce(function (result, key) {
-    //         return result && (header[key] !== -1);
-    //     }, true);
-    // }
-    //
-    // function generateUserData(header, contentArray) {
-    //     return contentArray.map(function (content) {
-    //         return {
-    //             utorid: content[header.utorid],
-    //             email: content[header.email],
-    //             studentNumber: content[header.studentNumber],
-    //             name: {firstName: content[header.firstName], lastName: content[header.lastName]}
-    //         };
-    //     });
-    // }
+            function _privilegeFilter(privilegeArray, searchValue) {
+                return privilegeArray.filter(function (privilegeObj) {
+                    return privilegeObj['name'] === searchValue;
+                })[0]._id; // filter the student privilege object and return the object id only
+            }
+        })
+        .then(function (courseData) {
+            var result = [];
+            userDataArray.forEach(function (userData) {
+                result.push(UserModel.updateOne({utorid: userData.utorid, studentNumber: userData.studentNumber}, // search by utorid and student number keys
+                    {$set: userData, $addToSet: {courseEnrolled: courseData}}, // add the course to courseEnrolled property
+                    {upsert: true}) // create new entry if student not found
+                    .then(function (user) {
+                        return true;
+                    })
+                    .catch(function (error) {
+                        return false;
+                    })
+                );
+            });
+            return Promise.all(result); // this solution returns an array of boolean indicating success/failed to save records to database
+        })
+        .then(function (resultArray) {
+            var num = resultArray.reduce(function (sum, boolResult) {
+                return sum + boolResult;
+            }, 0);
+            var response = 'Imported from student CSV file. Total ' + resultArray.length + ' records found: ' + num + ' students enrolled successfully.';
+            return res.send(response).end();
+        })
+        .catch(function (error) {
+            return res.status(500).send(error.message).end();
+        });
+
+    function parseCSVFile(csvString) {
+        var contentArray = csvString.split('\n').map(function (line) {
+            return line.trim('\r').split(',');
+        });
+        var headerArray = contentArray.shift().map(function (header) {
+            return header.toLowerCase();
+        });
+        return {header: headerArray, content: contentArray};
+    }
+
+    function parseCSVHeaders(headerArray) {
+        return headerArray.map(function (header) {
+            return header.includes(' ') ? _keyBuilder(header) : header;
+        });
+
+        function _keyBuilder(key) {
+            var keyArray = key.split(' ');
+            var first = keyArray[0];
+            var rest = keyArray.splice(1).map(function (str) {
+                return str.charAt(0).toUpperCase() + str.slice(1);
+            }).join('');
+            return first.concat(rest);
+        }
+    }
+
+    function isValidCSVHeaders(header) {
+        // 5 required keys for uploading csv file
+        return header.includes('utorid') && header.includes('email') && header.includes('studentNumber') && header.includes('firstName') && header.includes('lastName');
+    }
+
+    function parseUserData(header, contentArray) {
+        return contentArray.map(function (content) {
+            return _dataBuilder(header, content);
+        });
+
+        function _dataBuilder(keys, values) {
+            var d = keys.reduce(function (prev, val, i) {
+                prev[val] = values[i];
+                return prev;
+            }, {});
+            // merge name fields into one property
+            d.name = {firstName: d.firstName, lastName: d.lastName};
+            delete d.firstName;
+            delete d.lastName;
+            return d;
+        }
+    }
 
 });
 
 router.post('/:userID/avatar', uploadLocal.single('avatar'), function (req, res) {
-
+    // TODO
 });
 
 router.patch('/:userID/update/user-info', mw.checkAuthentication, function (req, res) {
